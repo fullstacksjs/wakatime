1a69e33f0d9a5c65e9a2d0b36e8b265871b9ddb8
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-09-26 11:46:14 +0330

    Validate LEADERBOARD_ID and API_URL in ValidateEnv function

--- packages/Screenshotreceiver/src/Config/Config.js
+++ packages/Screenshotreceiver/src/Config/Config.js
@@ -6,7 +6,8 @@ const DotEnvErrorChecker = () => {
     throw Error(`There was a problem reading the .env file.\n${error}`);
   }
 };
-const { API_KEY, PUPPETEER_EXECUTABLE_PATH } = process.env;
+const { API_KEY, PUPPETEER_EXECUTABLE_PATH, API_URL, LEADERBOARD_ID } =
+  process.env;
 
 const ValidateEnv = () => {
   if (!API_KEY) {
@@ -14,6 +15,12 @@ const ValidateEnv = () => {
   }
   if (!PUPPETEER_EXECUTABLE_PATH) {
     throw Error('PUPPETEER_EXECUTABLE_PATH is not defined in .env file');
+  }
+  if (!API_URL) {
+    throw Error('API_URL is not defined in .env file');
+  }
+  if (!LEADERBOARD_ID) {
+    throw Error('LEADERBOARD_ID is not defined in .env file');
   }
 };
 
