d15e73e24cdeae7fc00e0ede524ab78ce12dd177
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-09-30 20:57:29 +0330

    Added try catch for GetScreenShot function

--- packages/Screenshotreceiver/src/index.js
+++ packages/Screenshotreceiver/src/index.js
@@ -8,24 +8,29 @@ const PageQuery = async () => {
 };
 
 const GetScreenShot = async (Url) => {
-  const browser = await puppeteer.launch({
-    executablePath: PUPPETEER_EXECUTABLE_PATH,
-  });
+  try {
+    const browser = await puppeteer.launch({
+      executablePath: PUPPETEER_EXECUTABLE_PATH,
+    });
 
-  const Page = await browser.newPage();
+    const Page = await browser.newPage();
 
-  await Page.goto(`${Url.replace(/\/$/, '')}${await PageQuery()}`);
+    await Page.goto(`${Url.replace(/\/$/, '')}${await PageQuery()}`);
 
-  await Page.waitForTimeout(2000);
+    await Page.waitForTimeout(2000);
 
-  await Page.setViewport({ width: 815, height: 700, deviceScaleFactor: 2 });
+    await Page.setViewport({ width: 815, height: 700, deviceScaleFactor: 2 });
 
-  await Page.screenshot({
-    path: 'screenshot.jpg',
-    fullPage: true,
-  });
+    await Page.screenshot({
+      path: 'screenshot.jpg',
+      fullPage: true,
+    });
 
-  await browser.close();
+    await browser.close();
+  } catch (error) {
+    console.error(error);
+    process.exit();
+  }
 };
 
 GetScreenShot(WEBPAGE_URL);
