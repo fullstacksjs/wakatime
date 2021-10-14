c85105857889b75ffed97429b318b019477604f6
Hosein Pourdavar <h.pourdavar@gmail.com>
2021-10-14 22:28:35 +0330

    add getHours function

--- bot/src/utils/index.ts
+++ bot/src/utils/index.ts
@@ -1,5 +1,10 @@
-export default function weeksSince(dateString: string): number {
+export const weeksSince = (dateString: string): number => {
   var date = new Date(dateString).getMilliseconds();
   var today = new Date().getMilliseconds();
   return Math.floor((today - date) / (1000 * 60 * 60 * 24 * 7));
+}
+
+
+export const  getHours = (time: any) => {
+  return Math.floor(time / 3600).toString();
 }
