a4e68dcfe7d2372948a6d11079bc69d785814127
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-11-19 13:06:03 +0330

    feat: save schedule in schedule command

--- src/bot/commands/schedule.ts
+++ src/bot/commands/schedule.ts
@@ -1,3 +1,8 @@
 import { WakatimeContext } from '../Context.js';
 
-export const schedule = (ctx: WakatimeContext) => {};
+export const schedule = async (ctx: WakatimeContext) => {
+  if (ctx.schedule == null) return ctx.reply(ctx.messages.badSchedulePattern);
+  if (!ctx.isGroup()) return ctx.reply(ctx.messages.notAGroup);
+  await ctx.scheduleDb.addSchedule(String(ctx.chat!.id), ctx.schedule);
+  return ctx.reply(ctx.messages.scheduleSaved);
+};
