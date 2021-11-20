0ca01aee56e8a505fdb7a86e89b0b3ad46ad1fdb
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-11-20 20:25:05 +0330

    fix: fix parse mode

--- src/bot/useCases/createSendWeekly.ts
+++ src/bot/useCases/createSendWeekly.ts
@@ -7,5 +7,5 @@ export const createSendWeekly =
   (config: Config, repo: WakatimeRepo, api: Api) => async (groupId: GroupId) => {
     const leaderboard = await createLeaderboard(config, repo);
     const photo = new InputFile(leaderboard.image);
-    return api.sendPhoto(groupId, photo, { caption: leaderboard.caption });
+    return api.sendPhoto(groupId, photo, { caption: leaderboard.caption, parse_mode: 'HTML' });
   };
