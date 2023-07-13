a01dce4629d668466640b841c8ab80223432e7ee
Alireza Safaierad <frontendmonster@gmail.com>
2023-07-14 01:33:01 +0400

    remove web pages

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -2,10 +2,8 @@
 import type { Express } from 'express';
 import express from 'express';
 
-import { getReport } from './getReport.js';
-import { renderDayLeaderboard, renderWeekLeaderboard } from './renderLeaderboard.js';
 import type { Container } from '../config/initContainer.js';
-import { toAbsolutePath } from '../utils/path.js';
+import { getReport } from './getReport.js';
 
 export class Api {
   private config: Config;
@@ -15,16 +13,9 @@ export class Api {
     this.config = opts.config;
 
     this.app = express();
-    const viewPath = toAbsolutePath('/api/web');
-    this.app.set('view engine', 'ejs');
-    this.app.set('views', viewPath);
-
     this.app.use(express.json());
-    this.app.use(express.static(viewPath));
 
     this.app.get('/api/day', getReport);
-    this.app.get('/week', renderWeekLeaderboard);
-    this.app.get('/day', renderDayLeaderboard);
     this.app.get('/bail', (_, res) => res.status(200).end(''));
   }
 
