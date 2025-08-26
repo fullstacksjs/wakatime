d791bb914009a470acaceb0a2e47fe71a4ec7d18
Ali Hashemi <mr.ali.haashemi@gmail.com>
2025-08-27 01:46:50 +0330

    fix: disable the page cache

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 if (process.argv[2] === 'api') {
   const { Api } = await import('./api/Api.ts');
   const { registerApiContainer } = await import('./config/initContainer.ts');
+
   const container = await registerApiContainer();
+
   void new Api(container).start();
 }
 
 if (process.argv[2] === 'bot') {
   const { BotApi } = await import('./bot/BotApi.ts');
   const { registerBotContainer } = await import('./config/initContainer.ts');
+
   const container = await registerBotContainer();
+
   const bot = new BotApi(container);
+
   if (container.config.bot.webhookUrl) {
     void bot.registerWebhook(container.config.bot.webhookUrl);
   } else {
